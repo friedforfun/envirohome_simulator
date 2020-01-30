3420467f0363aa4f945293d5960bf8d38df999e6
chs <chs1@hw.ac.uk>
2020-01-30 12:06:36 +0000

    addedd TV and devices table + backrefs

--- services/web/project/models.py
+++ services/web/project/models.py
@@ -22,3 +22,38 @@ class User(db.Model):
 
     # def verify_password(self, password):
     #     return pwd_context.verify(password, self.password_hash)
+
+
+class Devices(db.Model):
+    __tablename__ = 'devices'
+    device_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    mac_addr = db.Column(db.String(17), nullable=True)
+    device_name = db.Column(db.String(64), nullable=False)
+    rated_power = db.Column(db.Integer, nullable=False)
+    device_type = db.Column(db.Enum('tv', 'plug', 'lights',
+                                    name="device_type"))
+    fault = db.Column(db.Boolean, default=False, nullable=False)
+    tv = db.relationship('TV', backref='device', uselist=False)
+
+
+class TV(db.Model):
+    tv_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    device_id = db.Column(db.Integer, db.ForeignKey('devices.device_id'))
+    channel = db.Column(db.Integer, default=1)
+    output = db.Column(db.Enum('HDMI1', 'HDMI2', 'ANT', 'DVD', 'VIDEO',
+                       name='channel'), default='HDMI1')
+    volume = db.Column(db.Integer, default=100)
+
+
+"""
+class Thermostat(db.Model):
+
+
+class Plug(db.Model):
+
+
+class Lights(db.Model):
+
+
+class Usage(db.Model):
+"""
