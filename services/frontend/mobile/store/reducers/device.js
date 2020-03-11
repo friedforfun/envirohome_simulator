5568357efc4a694d10b230e8955f9e76761a7ac9
Sam <sf52@hw.ac.uk>
2020-03-11 20:32:44 +0000

    Finished off device reducer

--- services/frontend/mobile/store/reducers/device.js
+++ services/frontend/mobile/store/reducers/device.js
@@ -10,11 +10,32 @@ const initialState = {
 const DeviceReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_DEVICE:
-
+            if (state.devices.length >= 1){
+                const newID = 1 + state.rooms.map(room => room.id).reduce((high, next) => { high > next ? high : next });
+                const newDevice = new Device(newID, action.deviceName, action.deviceType, 'false', '', 'false', action.rPower, action.deviceRoom);
+                return { ...state, devices: state.devices.concat(newDevice) };
+            } else {
+                const newDevice = new Device(0, action.deviceName, action.deviceType, 'false', '', 'false', action.rPower, action.deviceRoom);
+                return { ...state, devices: state.devices.concat(newDevice) };
+            }
+            
         case REMOVE_DEVICE:
+            const getIndex = state.devices.findIndex(devce => device.id === action.deviceID);
+            if (getIndex >= 0){
+                const tempDevices = [...state.devices]
+                tempDevices.splice(getIndex, 1);
+                return { ...state, devices: tempDevices }
+            } else {
+                return { ...state }
+            }
 
         case POPULATE_DEVICE:
             const newDevice = new Device(action.deviceId, action.deviceName, action.deviceType, action.deviceFault, '', action.deviceOn, action.rPower, action.deviceRoom);
+            if (state.devices.map(device => device.id).includes(newDevice.id)){
+                return { ...state };
+            } else {
+                return { ...state, devices: state.devices.concat(newDevice) };
+            }
             
     }
     return state;
