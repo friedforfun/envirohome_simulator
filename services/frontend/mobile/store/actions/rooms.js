97dd71f315c9c8cb5e28145ef1a0a131fb8d6f17
Sam <sf52@hw.ac.uk>
2020-03-11 10:41:57 +0000

    fix room action error

--- services/frontend/mobile/store/actions/rooms.js
+++ services/frontend/mobile/store/actions/rooms.js
@@ -3,8 +3,8 @@ export const ADD_ROOM = 'ADD_ROOM';
 export const REMOVE_ROOM = 'REMOVE_ROOM';
 
 // action functions
-export const addRoom = (id) => {
-    return { type: ADD_ROOM, roomID: id }
+export const addRoom = (name) => {
+    return { type: ADD_ROOM, roomName: name }
 };
 
 export const removeRoom = (id) => {
