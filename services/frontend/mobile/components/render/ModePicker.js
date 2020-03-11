cc811620a131a3d344f45748afbe9ce7b53527ab
Sam <sf52@hw.ac.uk>
2020-03-11 08:43:57 +0000

    Touchable Opacity on mode buttons

--- services/frontend/mobile/components/render/ModePicker.js
+++ services/frontend/mobile/components/render/ModePicker.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { ThemeProvider, Divider, Icon } from 'react-native-elements';
+import { StyleSheet, View, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { ThemeProvider, Icon } from 'react-native-elements';
 
 
 const ModePicker = props => {
@@ -11,30 +11,39 @@ const ModePicker = props => {
     */
     return (
         <View style={styles.content}>
-             <Icon 
-                //# source: https://github.com/primer/octicons#libraries
-                name="home"
-                type="octicon"
-                onPress={ props.mapView }
-                size={ 50 }
-            />
-            <Divider style={{ backgroundColor: 'blue' }}/>
-            <Icon 
-                //# source: https://github.com/primer/octicons#libraries
-                name="three-bars"
-                type="octicon"
-                onPress={ props.listView }
-                size={ 50 }
-            />
+            <TouchableHighlight style={styles.buttonAnim} >
+                <Icon 
+                    //# source: https://github.com/primer/octicons#libraries
+                    name="home"
+                    type="octicon"
+                    onPress={ props.mapView }
+                    
+                    size={ 50 }
+                />
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.buttonAnim}>
+                <Icon 
+                    //# source: https://github.com/primer/octicons#libraries
+                    name="three-bars"
+                    type="octicon"
+                    onPress={ props.listView }
+                    size={ 50 }
+                />
+            </TouchableHighlight>
         </View>
     );
-  }
+}
 
-  const styles = StyleSheet.create({
-      content:{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-      },
-  });
+const styles = StyleSheet.create({
+    content:{
+        flexDirection: 'row',
+
+
+    },
+    buttonAnim:{
+        flex: 1,
+        justifyContent: 'center',
+      }
+});
 
 export default ModePicker;
