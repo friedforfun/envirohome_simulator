4cf0006c71cac9d459d13a4753773788f162f5ac
Sam <sf52@hw.ac.uk>
2020-03-11 10:41:31 +0000

    Remove data parse logic from rooms reducer, add actions

--- services/frontend/mobile/store/reducers/rooms.js
+++ services/frontend/mobile/store/reducers/rooms.js
@@ -1,20 +1,31 @@
-import { DATA } from '../dummyData';
+import { ROOMDATA, DEVICEDATA } from '../dummyData';
+import { ADD_ROOM, REMOVE_ROOM } from '../actions/rooms';
+import Room from '../../models/room';
 
-//! functions to parse the temp dummy data ---
-const extractRoom = obj => {
-    return { title: obj.room }
-}
-const roomList = DATA.map(extractRoom);
-const uniqueRooms = Array.from(new Set(roomList));
- //! ---------------------------------------------
 
 
 //? Find a better initial state
 const initialState = {
-    rooms: uniqueRooms
+    rooms: ROOMDATA
 };
 
 const RoomReducer = (state = initialState, action) => {
+    switch (action.type){
+        case ADD_ROOM:
+            if (state.rooms.length >= 1){
+                const idArr = state.rooms.map(room => room.id);
+                const newID = 1 + idArr.reduce((high, next) => { high > next ? high : next });
+                const newRoom = new Room(newID, action.roomName);
+                return {...state, newRoom}
+            }
+            
+
+        case REMOVE_ROOM:
+            const getIndex = state.rooms.findIndex(room => room.id === action.roomID);
+            const tempRooms = [...state.rooms];
+            tempRooms.splice(getIndex, 1);
+            return { ...state, rooms: tempRooms }
+    }
     return state;
 }
 
