d4ca180855b9372a022afaa9192a0f27b73da677
harris <harris.adnan.ha@gmail.com>
2020-03-12 14:32:19 +0000

    2nd commit

---
+++ services/frontend/webui/src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+    return (
+        <Layout>
+        <div>
+            <h1>About Us</h1>
+            <p>Lorem40 sada sd asdasdasd asd asd asd asd as dasd as asdasda hehe</p>
+            <p>Now go build something great.</p>
+        </div>
+        </Layout>
+    )
+}
+
+export default AboutPage
+
--- services/frontend/webui/src/pages/index.js
+++ services/frontend/webui/src/pages/index.js
@@ -5,11 +5,12 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi pussies</h1>
+    <p>Welcome to the Eco Home Website</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
