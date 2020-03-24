4e99e9c9bf4f6baa8ede268d271139d375ca3194
Sam <sf52@hw.ac.uk>
2020-03-24 18:28:41 +0000

    Add basic styling to Utilisation display

--- services/frontend/mobile/components/render/Utilisation.js
+++ services/frontend/mobile/components/render/Utilisation.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { Platform, View, Text, StyleSheet } from 'react-native';
+import Colours from '../../constants/Colours';
 
 const Utilisation = props => {
     return (
         <View>
-            <Text>Energy Utility:</Text>
-            <Text>{props.usage}</Text>
+            <Text style={styles.textColour}>Energy Utility:</Text>
+            <Text style={styles.textColour}>{props.usage}</Text>
         </View>
     );
 
 }
+
+const styles = StyleSheet.create({
+    textColour: {
+        color: Platform.OS === 'android' ? 'white' : Colours.center
+    }
+});
+
 
 export default Utilisation;
