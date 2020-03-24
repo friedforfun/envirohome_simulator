241b7d6303aa541428717dbe09dd978e282db8c6
Sam <sf52@hw.ac.uk>
2020-03-24 18:29:17 +0000

    Implement Device rendering

--- services/frontend/mobile/screens/DeviceScreen.js
+++ services/frontend/mobile/screens/DeviceScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Divider, ListItem } from 'react-native-elements'
 import DeviceMenu from '../components/render/DeviceMenu';
 
 const DeviceScreen = props => {
@@ -7,12 +8,20 @@ const DeviceScreen = props => {
 
     return (
         <View style={styles.container}>
+            <ListItem 
+                key={'roomName'}
+                title={props.route.params.roomName}
+                titleStyle={{ fontWeight: 'bold'}}
+                chevron={{transform: [{rotate: '270deg'}]}}
+               
+            />
+            <Divider style={{backgroundColor: 'black', height:2}}/>
             <ScrollView style={styles.content}>
                 <DeviceMenu devices={props.route.params.deviceArray} />
             </ScrollView>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
