2a529bb8336cea7cfc26b9ac8fad4ca9a2387148
Sam <sf52@hw.ac.uk>
2020-03-24 18:30:00 +0000

    Parent Navigator component

--- services/frontend/mobile/navigation/Navigator.js
+++ services/frontend/mobile/navigation/Navigator.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
-import { RoomNavigator } from './RoomNavigator';
+
+import { RoomNavigator, LoginNavigator } from './AppNavigator';
+
 
 const Navigator = props =>{
+    // use selector from store to load authenticated or unauthenticated state
+    const isAuth = true;
+    const didTryAutoLogin = true;
+
+    //! need to add loading screen for when didTryAutoLogin is false
+    // {!isAuth && !didTryAutoLogin && <StartupScreen />}
     return (
         <NavigationContainer>
-            <RoomNavigator />
+            {isAuth && <RoomNavigator />}
+            {!isAuth && didTryAutoLogin && <LoginNavigator />}
         </NavigationContainer>
     );
 };
