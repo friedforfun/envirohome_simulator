8bdb98daae25806db900ab1c7a1a8990ae92b48e
Sam <sf52@hw.ac.uk>
2020-03-24 18:28:12 +0000

    Settings Icon WIP

--- services/frontend/mobile/components/render/SettingsIcon.js
+++ services/frontend/mobile/components/render/SettingsIcon.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
 import { Icon } from 'react-native-elements';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import Colours from '../../constants/Colours';
 
 const SettingsIcon = props => {
     return (
-        <View>
-            <Icon
-                //# source: https://github.com/primer/octicons#libraries
-                name="gear"
-                type="octicon"
-                onPress={props.settings}
-                size={50}
-            />
+        <View style={{flex: 1}}>
+                <Icon
+                    //# source: https://github.com/primer/octicons#libraries
+                    name="gear"
+                    type="octicon"
+                    color={Platform.OS === 'android' ? 'white' : Colours.center}
+                    onPress={props.settings}
+                    iconStyle={{height: '100%'}}
+                />                     
         </View>
     );
 
 }
+
 
 export default SettingsIcon;
