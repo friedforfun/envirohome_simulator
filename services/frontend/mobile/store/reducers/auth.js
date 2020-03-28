86fa9b3e094c3e50583b3d1869b79c5668e661c8
Sam <sf52@hw.ac.uk>
2020-03-28 18:08:55 +0000

    Refactor Auth reducer

--- services/frontend/mobile/store/reducers/auth.js
+++ services/frontend/mobile/store/reducers/auth.js
@@ -1,34 +1,44 @@
-import { AUTHENTICATE, LOGOUT, SET_DID_TRY_AL } from '../actions/auth';
+import { SIGNUP, AUTHENTICATE, LOGOUT, SET_DID_TRY_AL, REQUEST_PENDING, REQUEST_ERROR } from '../actions/auth';
 
 const initialState = {
-    token: null,
-    userId: null,
-    didTryAutoLogin: false
+    username: null,
+    email: null,
+    authToken: null,
+    data: null,
+    error: null
 };
  
-export default (state = initialState, action) => {
+export default AuthReducer = (state = initialState, action) => {
     switch (action.type) {
         case AUTHENTICATE:
             return {
-                token: action.token,
-                userId: action.userId,
-                didTryAutoLogin: true
+                data: action.payload,
             };
         case SET_DID_TRY_AL:
             return {
                 ...state,
-                didTryAutoLogin: true
             };
         case LOGOUT:
             return {
                 ...initialState,
-                didTryAutoLogin: true
             };
-        // case SIGNUP:
-        //   return {
-        //     token: action.token,
-        //     userId: action.userId
-        //   };
+        case SIGNUP:
+            console.log("Token:"+action.payload.data.user_id)
+            console.log("Auth token stored.")
+            return {
+                username: action.payload.config.username,
+                email: action.payload.config.email,
+                authToken:action.payload.data.user_id
+            };
+        case REQUEST_PENDING:
+            return {
+                ...state
+            }
+        case REQUEST_ERROR:
+            return {
+                ...state,
+                error: action.payload
+            }
         default:
             return state;
     }
