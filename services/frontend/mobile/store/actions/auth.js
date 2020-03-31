6df13cda507c23fcbf449e9c17ac7dc75c94d569
Sam <sf52@hw.ac.uk>
2020-04-01 00:33:00 +0100

    Login action

--- services/frontend/mobile/store/actions/auth.js
+++ services/frontend/mobile/store/actions/auth.js
@@ -1,11 +1,11 @@
 import { AsyncStorage } from 'react-native';
 
 export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
 export const REQUEST_PENDING = 'REQUEST_PENDING';
 export const REQUEST_ERROR = 'REQUEST_ERROR';
-// export const LOGIN = 'LOGIN';
-export const AUTHENTICATE = 'AUTHENTICATE';
-export const LOGOUT = 'LOGOUT';
 export const SET_DID_TRY_AL = 'SET_DID_TRY_AL';
 
 
@@ -33,75 +33,22 @@ export const authenticate = (userId, token, expiryTime) => {
 };
 
 export const signup = payload => {
-    console.log(payload)
-    console.log("ACTION. Auth Token: "+payload.data.user_id);
+    console.log(payload);
+    console.log("SIGNUP. Auth Token: "+payload.data.user_id);
     //console.log("signup action dispatched to store!")
     return {
         type: SIGNUP,
         payload: payload,
     }
 }
-    
 
-        
-    
-/*
-        if (!response.ok) {
-            const errorResData = await response.json();
-            const errorId = errorResData.error.message;
-            let message = 'Something went wrong!';
-            if (errorId === 'EMAIL_EXISTS') {
-                message = 'This email exists already!';
-            }
-            throw new Error(message);
-        }
-
-    const resData = await response.json();
-    console.log(resData);
-    dispatch(
-        authenticate(
-            resData.localId,
-            resData.idToken,
-            parseInt(resData.expiresIn) * 1000
-        )
-    );
-
-    const expirationDate = new Date(
-        new Date().getTime() + parseInt(resData.expiresIn) * 1000
-    );
-    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
-    */
-
-export const login = (email, password) => {
-    return async dispatch => {
-        const response = {} // login axios function here. return response
-
-        if (!response.ok) {
-            const errorResData = await response.json();
-            const errorId = errorResData.error.message;
-            let message = 'Something went wrong!';
-            if (errorId === 'EMAIL_NOT_FOUND') {
-                message = 'This email could not be found!';
-            } else if (errorId === 'INVALID_PASSWORD') {
-                message = 'This password is not valid!';
-            }
-            throw new Error(message);
-        }
-
-        const resData = await response.json();
-        console.log(resData);
-        dispatch(
-            authenticate(
-                resData.localId,
-                resData.idToken,
-                parseInt(resData.expiresIn) * 1000
-            )
-        );
-        const expirationDate = new Date(
-            new Date().getTime() + parseInt(resData.expiresIn) * 1000
-        );
-        saveDataToStorage(resData.idToken, resData.localId, expirationDate);
-    };
+export const login = payload => {
+    console.log(payload);
+    console.log("LOGIN. Auth Token: "+JSON.stringify(payload.data.token));
+    return {
+        type: LOGIN,
+        payload: payload
+    }
 };
 
 export const logout = () => {
