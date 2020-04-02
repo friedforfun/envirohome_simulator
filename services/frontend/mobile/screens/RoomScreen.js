e9072f1897347b330e3ba4cdbb6572fd2be9675c
Sam <sf52@hw.ac.uk>
2020-04-02 16:19:28 +0100

    Load room and device data

--- services/frontend/mobile/screens/RoomScreen.js
+++ services/frontend/mobile/screens/RoomScreen.js
@@ -1,21 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { useSelector } from 'react-redux';
+import Spinner from 'react-native-loading-spinner-overlay';
+
 
 import RoomMenu from '../components/render/RoomMenu';
+import GetAllRooms from '../components/logic/GetAllRooms';
+
+import { populateRooms } from '../store/actions/rooms';
+import { useDispatch } from 'react-redux';
+
+
+
 
 const RoomScreen = props => {
-    const roomList = useSelector(state => state.roomStore.rooms);
+    const [isLoading, setIsLoading] = useState(true);
+    let roomList = useSelector(state => state.roomStore.rooms)
+    let done =  false;
+
+    const dispatch = useDispatch();
+
+    const toggleLoading = () => {
+        console.log("done")
+        setIsLoading(true ? false : true)
+    }
+
+    useEffect(() => {
+        
+        GetAllRooms().then(response => {
+            console.log("dispatching...");
+            dispatch(populateRooms(response));
+            return response;
+        }).catch(error => {
+            console.log("Error here")
+            console.log(error)}
+        ).finally(() => {
+            done = true
+        });
+        return () => toggleLoading();
+    }, [])
+    
 
     return (
         <View style={styles.container}>
+            <Spinner
+                visible={isLoading}
+                textContent={'Loading...'}
+            />
             <ScrollView style={styles.content}>
-                <RoomMenu navigation={props.navigation} rooms={roomList} />
+                {!isLoading && <RoomMenu navigation={props.navigation} rooms={roomList} />}
             </ScrollView>
         </View>
     );
 }
-
+ 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
