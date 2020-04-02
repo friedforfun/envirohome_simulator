09404da01514548f95f3e0b491196abd46af7686
Sam <sf52@hw.ac.uk>
2020-04-02 23:25:51 +0100

    Remove axios

--- services/frontend/mobile/components/logic/RegisterUser.js
+++ services/frontend/mobile/components/logic/RegisterUser.js
@@ -1,28 +1,24 @@
-import axios from 'axios';
-
 import URL from '../../constants/URL';
 
+
 const RegisterUser = (user, pword, email) => {
-  const register = axios.create({
-    baseURL: URL.base,
+  const path = URL.base + URL.auth + URL.register;
+
+  return fetch(path, {
+    method: "POST",
     headers: {
       'Content-Type': 'application/json',
       'Connection': 'close'
-    }
-  });
-
-  const path = URL.auth + URL.register;
-
-  return register({
-    method: 'post',
-    timeout: 8000,
-    url: path,
-    data: {
+    },
+    body: JSON.stringify({
       username: user,
       password: pword,
       email: email
-    }
-  })
-}
+    })
+  }).then(response => {
+      if (!response.ok) throw Error(response.statusText);
+      return response.json();
+    })
+} 
 
 export default RegisterUser;
