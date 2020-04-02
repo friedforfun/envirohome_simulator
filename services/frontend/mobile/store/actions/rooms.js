017b7d088f04415c9d902387e5ad05a7ce4a1868
Sam <sf52@hw.ac.uk>
2020-04-02 16:19:43 +0100

    Added populate rooms action

--- services/frontend/mobile/store/actions/rooms.js
+++ services/frontend/mobile/store/actions/rooms.js
@@ -2,6 +2,7 @@
 export const ADD_ROOM = 'ADD_ROOM';
 export const REMOVE_ROOM = 'REMOVE_ROOM';
 export const ADD_DEVICE_TO_ROOM = 'ADD_DEVICE_TO_ROOM';
+export const POPULATE_ROOMS = 'POPULATE_ROOMS';
 
 // action functions
 export const addRoom = (name) => {
@@ -14,4 +15,8 @@ export const removeRoom = (id) => {
 
 export const addDeviceToRoom = (roomId, device) => {
     return { type: ADD_DEVICE_TO_ROOM, roomID: roomId, deviceObj: device};
+}
+
+export const populateRooms = (roomArr) => {
+    return { type: POPULATE_ROOMS, roomArray: roomArr };
 }
