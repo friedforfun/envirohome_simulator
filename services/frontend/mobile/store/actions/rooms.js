087a71db0c23cffc9cf30f5804e8d391694179b8
Sam <sf52@hw.ac.uk>
2020-04-05 22:34:56 +0100

    remove device from room action & reducer

--- services/frontend/mobile/store/actions/rooms.js
+++ services/frontend/mobile/store/actions/rooms.js
@@ -3,6 +3,7 @@ export const ADD_ROOM = 'ADD_ROOM';
 export const REMOVE_ROOM = 'REMOVE_ROOM';
 export const ADD_DEVICE_TO_ROOM = 'ADD_DEVICE_TO_ROOM';
 export const POPULATE_ROOMS = 'POPULATE_ROOMS';
+export const REMOVE_DEVICE_FROM_ROOM = 'REMOVE_DEVICE_FROM_ROOM';
 
 // action functions
 export const addRoom = (name) => {
@@ -15,6 +16,10 @@ export const removeRoom = (id) => {
 
 export const addDeviceToRoom = (roomId, device) => {
     return { type: ADD_DEVICE_TO_ROOM, roomID: roomId, deviceObj: device};
+}
+
+export const removeDeviceFromRoom = (roomId, device) => {
+    return { type: REMOVE_DEVICE_FROM_ROOM, roomID: roomId, deviceObj: device };
 }
 
 export const populateRooms = (roomArr) => {
