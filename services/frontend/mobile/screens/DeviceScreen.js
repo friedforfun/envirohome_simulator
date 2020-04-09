c06633b6e8ca2baaabb5379fe46a91a49a709d72
Sam <sf52@hw.ac.uk>
2020-04-09 19:12:38 +0100

    use redux for devices

--- services/frontend/mobile/screens/DeviceScreen.js
+++ services/frontend/mobile/screens/DeviceScreen.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { Divider, ListItem } from 'react-native-elements'
+import { useSelector } from 'react-redux';
+
 import DeviceMenu from '../components/render/DeviceMenu';
 
 const DeviceScreen = props => {
     //console.log(props)
+
+    const deviceStore = useSelector(state => state.deviceStore.devices);
+    const deviceArr = deviceStore.filter(device => device.room === props.route.params.roomName)
 
     return (
         <View style={styles.container}>
             <ListItem 
                 key={'roomName'}
                 title={props.route.params.roomName}
+                subtitle={"Power: "+props.route.params.roomPower}
                 titleStyle={{ fontWeight: 'bold'}}
                 chevron={{transform: [{rotate: '270deg'}]}}
                
             />
             <Divider style={{backgroundColor: 'black', height:2}}/>
             <ScrollView style={styles.content}>
-                <DeviceMenu devices={props.route.params.deviceArray} />
+                <DeviceMenu devices={deviceArr} roomName={props.route.params.roomName} />
             </ScrollView>
         </View>
     );
