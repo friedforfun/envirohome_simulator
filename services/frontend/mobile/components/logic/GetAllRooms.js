2a29d75e3eb6f1a53fa1936e3753b9d36d70a173
Sam <sf52@hw.ac.uk>
2020-04-12 00:53:27 +0100

    fix fetch route

--- services/frontend/mobile/components/logic/GetAllRooms.js
+++ services/frontend/mobile/components/logic/GetAllRooms.js
@@ -1,40 +1,10 @@
-import React from 'react';
-
 import URL from '../../constants/URL';
-import Device from '../../models/device';
-import Room from '../../models/room';
 
-// converts fetched device data into device object
-export const toDevice = obj => {
-    return new Device(obj.device_id, obj.device_name, obj.device_type, obj.fault, obj.mac_addr, obj.on, obj.rated_power, obj.room)
-};
+const GetAllRooms = async () => {
 
-// creates room object from device array
-export const toRoom = (element, index) => { return new Room(index, element) };
+    const path = URL.base + URL.api + URL.room;
 
-// maps device array to rooms
-export const mapDeviceToRoom = (room, devices) => {
-    room.deviceArray = devices.filter(device => device.room === room.name);
-    return room;
-}
-
-const GetAllRooms = () => {
-    let deviceData;
-
-    const path = URL.base + URL.api + URL.alldevices;
-
-    return fetch(path).then(response => response.json())
-    .then(response => {
-        deviceData = response;
-        return response;
-    }).then(response => {
-        return [...new Set(response.map(item => item.room))]
-    }).then(response => {
-        return response.map(toRoom)
-    }).then(response => {
-        let rooms = response.map(room => mapDeviceToRoom(room, deviceData))
-        return rooms;
-    })
+    return await fetch(path)
 
 }
 
