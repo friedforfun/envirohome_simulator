6d038efe62e1e7b55b18840b833bc5dcde482465
Sam <sf52@hw.ac.uk>
2020-04-18 13:56:49 +0100

    Chart data fetcher

--- services/frontend/mobile/components/logic/DevicePower.js
+++ services/frontend/mobile/components/logic/DevicePower.js
@@ -1,9 +1,53 @@
 import URL from '../../constants/URL';
+import {testResponse} from './fetchFunc';
 
+export const fetchHead = async (deviceId) => {
 
-const DevicePower = async (deviceId) => {
+    return await DevicePower(deviceId)
+        .then(response => {
+            return testResponse(response)
+        })
+        .then(ok => {
+            return ok.json()
+        })
+        .catch(error => {
+            console.log(error.message)
+        })
 
-    const path = URL.eventStore + URL.streams + URL.deviceUsage + deviceId + URL.head
+}
+
+export const fetchNext = async (deviceId, uriSuffix) => {
+
+    return await DevicePower(deviceId, false, uriSuffix)
+        .then(response => {
+            return testResponse(response)
+        })
+        .then(ok => {
+            return ok.json()
+        })
+        .catch(error => {
+            console.log(error.message)
+        })
+
+}
+
+export const fetchFirst = async (deviceId) => {
+
+    return await DevicePower(deviceId, false, 0)
+        .then(response => {
+            return testResponse(response)
+        })
+        .then(ok => {
+            return ok.json()
+        })
+        .catch(error => {
+            console.log(error.message)
+        })
+}
+
+const DevicePower = async (deviceId, getHead = true, uriSuffix = 0) => {
+
+    const path = getHead ? URL.eventStore + URL.streams + URL.deviceUsage + deviceId + URL.head : URL.eventStore + URL.streams + URL.deviceUsage + deviceId + "/" + uriSuffix;
 
     var myHeaders = new Headers();
     myHeaders.append("Accept", "application/vnd.eventstore.atom+json");
