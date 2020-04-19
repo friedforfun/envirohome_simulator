3652121f3f323a21f576d348f41822e7bfa4dd3b
Sam <sf52@hw.ac.uk>
2020-04-19 22:02:14 +0100

    OSX launch script changes

--- OSXlaunch.sh
+++ OSXlaunch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-## export HOST_IP=$(ifconfig | grep inet | grep -v -e 'inet6'  -e '127.0.0.1' | awk '{print $2}') 
-export HOST_IP=$(ifconfig $(ip route show | grep ^default | cut -d" " -f5) | grep inet[^0-9] | tr -s ' ' | cut -d" " -f3)
+export HOST_IP=$(ifconfig | grep inet | grep -v -e 'inet6'  -e '127.0.0.1' | awk '{print $2}') 
+##export HOST_IP=$(ifconfig $(ip route show | grep ^default | cut -d" " -f5) | grep inet[^0-9] | tr -s ' ' | cut -d" " -f3)
 sed -i '' -e "s/HOST_IP=.*/HOST_IP=$HOST_IP/" .env
 sed -i '' -e "s/HOST_IP=.*/HOST_IP=$HOST_IP/" services/frontend/mobile/.env
 exec docker-compose build
