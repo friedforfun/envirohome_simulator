740cf75d6f04e3e7bc90dcbafc6d1d34ce0eba4b
Sam <sf52@hw.ac.uk>
2020-04-20 01:18:40 +0100

    Launch script with args

--- OSXlaunch.sh
+++ OSXlaunch.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-export HOST_IP=$(ifconfig | grep inet | grep -v -e 'inet6'  -e '127.0.0.1' | awk '{print $2}') 
-##export HOST_IP=$(ifconfig $(ip route show | grep ^default | cut -d" " -f5) | grep inet[^0-9] | tr -s ' ' | cut -d" " -f3)
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, attempting to automate. please provide your machines ip address as an argument next time."
+    export HOST_IP=$(ifconfig | grep inet | grep -v -e 'inet6'  -e '127.0.0.1' | awk '{print $2}') 
+    ##export HOST_IP=$(ifconfig $(ip route show | grep ^default | cut -d" " -f5) | grep inet[^0-9] | tr -s ' ' | cut -d" " -f3)
+  else
+    export HOST_IP="$1"
+fi
+
 sed -i '' -e "s/HOST_IP=.*/HOST_IP=$HOST_IP/" .env
 sed -i '' -e "s/HOST_IP=.*/HOST_IP=$HOST_IP/" services/frontend/mobile/.env
 exec docker-compose build
--- launch.sh
+++ launch.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-export HOST_IP=$(ip -oneline -family inet address | awk '!/127.0.0.1/{print substr($4, 1, length($4)-3)}' | head --lines=1)
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, attempting to automate. please provide your machines ip address as an argument if this doesnt work..."
+    export HOST_IP=$(ip -oneline -family inet address | awk '!/127.0.0.1/{print substr($4, 1, length($4)-3)}' | head --lines=1)
+  else
+    export HOST_IP="$1"
+fi
+
 sed -i "s/HOST_IP=.*/HOST_IP=$HOST_IP/" .env
 sed -i "s/HOST_IP=.*/HOST_IP=$HOST_IP/" services/frontend/mobile/.env
 exec docker-compose build
