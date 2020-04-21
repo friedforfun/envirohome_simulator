8e3fd367149c0c80d89682e2a5114b2b417429c9
Sam <sf52@hw.ac.uk>
2020-04-21 16:31:04 +0100

    Household grade

--- services/frontend/mobile/components/render/UtilisationHeader.js
+++ services/frontend/mobile/components/render/UtilisationHeader.js
@@ -22,7 +22,7 @@ const UtilisationHeader = props => {
                 console.log(error.message)
             })
 
-    }, 5000);
+    }, 4000);
 
     const updateUsage = data => {
         setUsage(data)
@@ -41,11 +41,56 @@ const UtilisationHeader = props => {
 
     const percentage = (usage / props.maxRatedPower)
 
+    
+
+    const grade = () => {
+
+        
+        // A+ > 0.9
+        // A  <= 0.9 && > 0.8
+        // B  <= 0.8 && > 0.7
+        // C  <= 0.7 && > 0.5
+        // D  <= 0.5 && > 0.3
+        // E  <= 0.3 && > 0.2
+        // F  <= 0.2 && > 0.1
+        // G  <= 0.1
+
+        const inversePercentage = 1 - percentage
+        switch (true) {
+            case  (inversePercentage > 0.9):
+                return "A+"
+
+            case (inversePercentage <= 0.9 && inversePercentage > 0.8):
+                return "A"
+            
+            case (inversePercentage <= 0.8 && inversePercentage > 0.7):
+                return "B"
+            
+            case (inversePercentage <= 0.7 && inversePercentage > 0.5):
+                return "C"
+
+            case (inversePercentage <= 0.5 && inversePercentage > 0.3):
+                return "D"
+
+            case (inversePercentage <= 0.3 && inversePercentage > 0.2):
+                return "E"
+
+            case (inversePercentage <= 0.2 && inversePercentage > 0.1):
+                return "F"
+
+            case (inversePercentage <= 0.1):
+                return "G"
+
+            default:
+                return "U"
+                break;
+        }
+    }
+
     return (
         <Grid {...props} style={styles.gridStyle}>
             <Col style={styles.columnStyle}>
-                <Text style={styles.textColour}>Energy Utility: {Math.trunc(100-((usage/props.maxRatedPower)*100))}%</Text>
-                <Text style={styles.textColour}>Max rated power: {props.maxRatedPower}</Text>
+                <Text style={styles.textColour}>Energy Grade: {grade()}</Text>
                 <UtilisaionBar
                     value={percentage}
                     height={14}
@@ -70,13 +115,18 @@ UtilisationHeader.propTypes = {
 
 const styles = StyleSheet.create({
     textColour: {
-        color: Platform.OS === 'android' ? 'white' : Colours.center
+        color: Platform.OS === 'android' ? 'white' : Colours.center,
+        alignSelf: 'center'
     },
     columnStyle: {
+        justifyContent: 'center',
+        alignContent: 'center',
         width: '100%'
     },
     gridStyle: {
         width: 250,
+        justifyContent: 'center',
+        alignContent: 'center',
         
     }
 
